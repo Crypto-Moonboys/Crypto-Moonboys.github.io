b203784ef50909bbc25d2901d4218a3d7cd25eb3
HODLKONG64 <hodlkong64@gmail.com>
2026-04-07 03:18:46 +0100

    BDFB

--- js/js/ranking-config.js
+++ js/js/ranking-config.js
@@ -13,6 +13,25 @@ module.exports = {
     description: 10,
     category: 5,
     word_count: 0.05,
-    keyword_bag: 1
+    keyword_bag: 1,
+    authority: 1
+  },
+
+  AUTHORITY: {
+    internal_links: {
+      tier_1: 12,
+      tier_2: 8,
+      tier_3: 4
+    },
+    title_depth: {
+      tier_1: 8,
+      tier_2: 5,
+      tier_3: 2
+    },
+    metadata: {
+      keywords_bonus: 4,
+      headings_bonus: 4,
+      lists_bonus: 2
+    }
   }
 };
