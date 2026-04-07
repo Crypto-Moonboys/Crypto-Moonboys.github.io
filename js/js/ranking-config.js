6ab1594030a906e5954ac97bdcd3d3f33126cfeb
HODLKONG64 <hodlkong64@gmail.com>
2026-04-07 03:03:46 +0100

    NOL

---
+++ js/js/ranking-config.js
@@ -0,0 +1,18 @@
+window.RANKING_CONFIG = {
+  CATEGORY_PRIORITY: {
+    core: 10,
+    characters: 8,
+    factions: 7,
+    tokens: 6,
+    concepts: 5,
+    misc: 3
+  },
+
+  WEIGHTS: {
+    canonical: 20,
+    description: 10,
+    category: 5,
+    word_count: 0.1,
+    keyword_bag: 1
+  }
+};
