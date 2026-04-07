a10bcd127cd437eb5f7f26c62ead07f3aadffdc8
HODLKONG64 <hodlkong64@gmail.com>
2026-04-07 03:06:36 +0100

    OJO

--- js/js/ranking-config.js
+++ js/js/ranking-config.js
@@ -1,4 +1,4 @@
-window.RANKING_CONFIG = {
+module.exports = {
   CATEGORY_PRIORITY: {
     core: 10,
     characters: 8,
@@ -12,7 +12,7 @@ window.RANKING_CONFIG = {
     canonical: 20,
     description: 10,
     category: 5,
-    word_count: 0.1,
+    word_count: 0.05,
     keyword_bag: 1
   }
 };
