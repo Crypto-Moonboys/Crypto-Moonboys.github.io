d1d1435f914cefa988e50cba9378aa7010a9b4d0
HODL KONG 64 <hodlkong64@gmail.com>
2026-07-22 18:35:31 +0100

    fix(invaders): lower alert sounds another 50 percent

--- js/arcade/games/invaders/meme-visual-hotfix.js
+++ js/arcade/games/invaders/meme-visual-hotfix.js
@@ -192,7 +192,7 @@
     try {
       audio.pause();
       audio.currentTime = 0;
-      audio.volume = 0.375;
+      audio.volume = 0.1875;
       audio.play().catch(function () {});
     } catch (_) {
       // Audio can fail before first user gesture on some browsers; ignore safely.
@@ -216,7 +216,7 @@
     try {
       audio.pause();
       audio.currentTime = 0;
-      audio.volume = 0.39;
+      audio.volume = 0.195;
       audio.play().catch(function () {});
     } catch (_) {
       // Audio can fail before first user gesture on some browsers; ignore safely.
