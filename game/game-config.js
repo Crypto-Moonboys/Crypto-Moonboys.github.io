1bb4b5c4cf21a3c7a9457b70230c40770abe57fa
HODL KONG 64 <hodlkong64@gmail.com>
2026-08-23 05:32:47 +0100

    Add NBG game runtime configuration

---
+++ game/game-config.js
@@ -0,0 +1,15 @@
+export const GAME_CONFIG = {
+  title: "NBG London Graffiti Run",
+  level: "london-01",
+  width: 320,
+  height: 180,
+  pixelScale: 3,
+  xpName: "XP",
+  systems: {
+    player: true,
+    camera: true,
+    coins: true,
+    enemies: true,
+    leaderboard: true
+  }
+};
