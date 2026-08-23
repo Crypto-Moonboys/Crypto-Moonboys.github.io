57a27a2d2b2fffc3f95d8e8abcb492b11f14dbfe
HODL KONG 64 <hodlkong64@gmail.com>
2026-08-23 06:02:59 +0100

    Add Level 1 master render composer

---
+++ game/engine/level1-master-render-composer.js
@@ -0,0 +1,27 @@
+// Level 1 Master Render Composer
+// Combines all visual layers into one frame.
+
+class Level1MasterRenderComposer {
+  constructor(renderer) {
+    this.renderer = renderer;
+    this.layers = [];
+  }
+
+  addLayer(layer) {
+    this.layers.push(layer);
+  }
+
+  init() {
+    return this.layers.length;
+  }
+
+  render(state) {
+    for (const layer of this.layers) {
+      if (layer && typeof layer.render === 'function') {
+        layer.render(state);
+      }
+    }
+  }
+}
+
+export default Level1MasterRenderComposer;
