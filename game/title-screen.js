b7bd8e9f0e045c0ed0cc699b996b6dfe28299f76
HODL KONG 64 <hodlkong64@gmail.com>
2026-08-23 05:41:11 +0100

    Add NBG game title screen flow

---
+++ game/title-screen.js
@@ -0,0 +1,10 @@
+export const titleScreen = {
+  title: 'NBG LONDON GRAFFITI RUN',
+  subtitle: 'RUN • TAG • EARN XP',
+  startButton: 'START LEVEL 1',
+  leaderboardButton: 'LEADERBOARD'
+};
+
+export function startGame(callback) {
+  if (callback) callback();
+}
