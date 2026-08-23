bd4a50fca2b0a3fa382de91296e7f209d7bbed3f
HODL KONG 64 <hodlkong64@gmail.com>
2026-08-23 16:37:32 +0100

    Connect Level 1 scene to browser runtime

--- game/level1/level1-scene.js
+++ game/level1/level1-scene.js
@@ -1,18 +1,27 @@
 // Level 1 London Graffiti scene
+// Browser-compatible scene runtime.
 
-export class LondonGraffitiScene {
+class LondonGraffitiScene {
   constructor(loader, renderer) {
     this.loader = loader;
     this.renderer = renderer;
     this.entities = [];
+    this.loaded = false;
   }
 
   async load() {
     this.entities = await this.loader.load('level1-london.json');
+    this.loaded = true;
     return this;
   }
 
   update(delta) {
     this.entities.forEach(entity => entity.update?.(delta));
   }
+
+  render(context, assets) {
+    this.renderer?.render?.(context, this.entities, assets);
+  }
 }
+
+window.LondonGraffitiScene = LondonGraffitiScene;
